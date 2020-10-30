1c85b4a7d1654dd7d45ac1063652284d275ea725
allgemein <amanetamada@gmail.com>
2020-10-30 12:16:58 +0900

    indexを作ったりcameraを微修正したりした

--- clothes/urls.py
+++ clothes/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('camera/', include('camera.urls')),
+    path('users/', include('users.urls')),
+    path('users/camera/', include('camera.urls')),
 ]
--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('shoot/', views.shoot, name='shoot'),
+    path('index', views.index, name='index'),
 ]
